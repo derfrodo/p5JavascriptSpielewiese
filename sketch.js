f79be9bd3e57ba206a2c72ff1b29ad0cab5040a1
derfrodo <derfrodo0715@gmail.com>
2017-03-04 01:40:43 +0100

    Wir haben gears!

--- sketch.js
+++ sketch.js
@@ -1,27 +1,59 @@
-function setup(){
-    createCanvas(640,480);
+function setup() {
+    createCanvas(640, 480);
     angleMode(DEGREES);
 }
 
-function draw(){
+function draw() {
 
-fill(255);
+    fill(255);
     background(0);
     // text(`mouse: ${mouseX}`, 100, 100);
 
-    var angle = mouseX % 360;
-    push();
-    translate(50,50);
-    rotate(angle);
-rectMode(CENTER);
-rect(0,0,30,30);
-    pop();
+    var angle = (mouseX / 2) % (2 * PI);
+
+    var cogs = 15;
+    const additionalAngle = PI / cogs;
+    drawGear(200, 200, 40, cogs, angle + additionalAngle);
+    drawGear(246, 200, 40, cogs, -1 * angle);
 
     push();
-    translate(90,50);
-    rotate((angle + 45) * -1);
-rectMode(CENTER);
-rect(0,0,30,30);
-    pop();
 
+    translate (100,100);
+    rotate((second()/60) * (2*PI));
+    textAlign(CENTER, CENTER);
+    text(second(),0,0);
+    pop();
+}
+
+function drawGear(x, y, radius, cogs, initialAngle) {
+
+    push();
+    translate(x, y);
+        rectMode(CORNER);
+
+    angleMode(RADIANS);
+    rotate(initialAngle);
+    fill(255);
+    ellipse(0, 0, radius, radius);
+    
+    stroke(0, 0, 255);
+    fill(0, 0, 255);
+    textSize(radius);
+    textAlign(CENTER, CENTER);
+    text("G", 0,0);
+
+    const angle = (PI * 2) / cogs;
+    const cogSize = radius / cogs;
+
+    for (let i = 0; i < cogs; i++) {
+        push();
+        rotate(i * angle);
+        translate(0, (radius / 2) - 1);
+        noStroke();
+
+        rect(-1 * (cogSize / 2), 0, cogSize, cogSize * 2);
+        pop();
+    }
+
+    pop();
 }
